662e339198b24a1805baf763de72d460b85eeb36
florenciaaudry <florenciaaudry06@gmail.com>
2026-05-06 23:54:27 +0700

    Delete budijayaweb directory

--- budijayaweb/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/auth_admin.php';
-session_destroy();
-header('Location: login.php');
-exit;
--- budijayaweb/logout_customer.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-unset($_SESSION['customer']);
-session_write_close();
-header('Location: index.php');
-exit;
